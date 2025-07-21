b1198678c4b9f640b0265ebce58bbd8a222f38a9
kinsengchan <chan_kin_seng@hotmail.com>
2025-07-21 09:50:19 +0800

    Initialise Spring Boot App

---
+++ src/main/java/team2/kakigowherebackend/KakiGoWhereBackendApplication.java
@@ -0,0 +1,13 @@
+package team2.kakigowherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakiGoWhereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakiGoWhereBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team2/kakigowherebackend/KakiGoWhereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.kakigowherebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakiGoWhereBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
