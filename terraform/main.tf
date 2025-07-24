1254a8f2983b01e8673eafc49ab95ee9e3fa624a
imksc <chan_kin_seng@hotmail.com>
2025-07-25 01:39:22 +0800

    feat: Cd pipeline (#4)
    * chore: split dev and prod application.properties
    
    * chore: setup cd pipeline
    
    * chore: setup terraform
    
    * fix: add working directory for building docker
    
    * fix: fix terraform var
    
    * chore: setup ansible
    
    * fix: for get droplet ip
    
    * fix: Save droplet ip in GITHUB_OUTPUT
    
    * fix: Changed Droplet name
    
    * fix: added [app] for making ansible inventory
    
    * fix: cache ansible
    
    * chore: docker-compose to pull new images and restart
    
    * chore: final ci/cd pipeline except cd tests

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.60.0"
+    }
+  }
+}
+
+terraform {
+  cloud {
+
+    organization = "Team2_SA60"
+
+    workspaces {
+      name = "KakiGoWhere"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "app" {
+  name       = "KakiGoWhere"
+  region     = "sgp1"
+  image      = "ubuntu-24-04-x64"
+  size       = "s-2vcpu-2gb"
+  ssh_keys   = [var.ssh_key_fingerprint]
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.app.ipv4_address
+}
