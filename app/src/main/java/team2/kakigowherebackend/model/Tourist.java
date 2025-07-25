732f4b0b41a275e52442c7fb1f4a6141775e1165
vegecloud <caiyun_yap@hotmail.com>
2025-07-25 15:54:20 +0800

    #1a Add class: Tourist class

---
+++ app/src/main/java/team2/kakigowherebackend/model/Tourist.java
@@ -0,0 +1,27 @@
+package team2.kakigowherebackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Tourist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int touristId;
+    private String touristEmail;
+    private String touristPassword;
+    private String touristName;
+
+    public Tourist() { }
+    public Tourist(String touristEmail, String touristPassword, String touristName) {
+        this.touristEmail = touristEmail;
+        this.touristPassword = touristPassword;
+        this.touristName = touristName;
+    }
+}
