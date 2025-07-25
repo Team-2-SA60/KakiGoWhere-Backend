0d3571864e9c7dd2d7b38514785de69a80b55bea
vegecloud <caiyun_yap@hotmail.com>
2025-07-25 17:24:37 +0800

    #1e Add class: ItineraryDetail class

--- app/src/main/java/team2/kakigowherebackend/model/Itinerary.java
+++ app/src/main/java/team2/kakigowherebackend/model/Itinerary.java
@@ -1,11 +1,17 @@
 package team2.kakigowherebackend.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
 public class Itinerary {
 
     @Id
---
+++ app/src/main/java/team2/kakigowherebackend/model/ItineraryDetail.java
@@ -0,0 +1,30 @@
+package team2.kakigowherebackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class ItineraryDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long itineraryDetailId;
+    private LocalDate itemDate;
+    private String itemNotes;
+    private int sequentialOrder;
+
+    public ItineraryDetail() { }
+    public ItineraryDetail(LocalDate itemDate, String itemNotes, int sequentialOrder) {
+        this.itemDate = itemDate;
+        this.itemNotes = itemNotes;
+        this.sequentialOrder = sequentialOrder;
+    }
+}
