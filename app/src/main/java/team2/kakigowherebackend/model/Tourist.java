42a7d0723e47ea19cd8fee8e32a5a95ef36b05ce
im-ksc <chan_kin_seng@hotmail.com>
2025-07-28 21:10:56 +0800

    chore: applied auto code formatting

--- app/src/main/java/team2/kakigowherebackend/model/Tourist.java
+++ app/src/main/java/team2/kakigowherebackend/model/Tourist.java
@@ -2,10 +2,9 @@ package team2.kakigowherebackend.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @Getter
@@ -15,18 +14,19 @@ public class Tourist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
     private String password;
     private String name;
 
-    @OneToOne
-    private Itinerary itinerary;
+    @OneToOne private Itinerary itinerary;
 
     @ManyToMany
     @Size(max = 3)
-    @JoinTable(name = "tourist_interests",
-    joinColumns = @JoinColumn(name = "tourist_id"),
-    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @JoinTable(
+            name = "tourist_interests",
+            joinColumns = @JoinColumn(name = "tourist_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private List<InterestCategory> interestCategories;
 
     @OneToMany
@@ -37,7 +37,8 @@ public class Tourist {
     @JoinColumn(name = "tourist_id")
     private List<Rating> ratings;
 
-    public Tourist() { }
+    public Tourist() {}
+
     public Tourist(String email, String password, String name) {
         this.email = email;
         this.password = password;
--- app/src/main/java/team2/kakigowherebackend/repository/PlaceRepository.java
+++ app/src/main/java/team2/kakigowherebackend/repository/PlaceRepository.java
@@ -3,6 +3,4 @@ package team2.kakigowherebackend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team2.kakigowherebackend.model.Place;
 
-public interface PlaceRepository extends JpaRepository<Place, Long> {
-
-}
+public interface PlaceRepository extends JpaRepository<Place, Long> {}
