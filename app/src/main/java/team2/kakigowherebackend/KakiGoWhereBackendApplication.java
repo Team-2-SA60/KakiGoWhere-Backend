c1f4129052ab2d8263622a4fd6e406b62c31893e
vegecloud <caiyun_yap@hotmail.com>
2025-07-29 13:33:14 +0800

    Update Application.java: added mock data in commandLineRun.

--- app/src/main/java/team2/kakigowherebackend/KakiGoWhereBackendApplication.java
+++ app/src/main/java/team2/kakigowherebackend/KakiGoWhereBackendApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import team2.kakigowherebackend.model.Place;
+import team2.kakigowherebackend.service.PlaceService;
+
+import java.time.LocalTime;
 
 @SpringBootApplication
 public class KakiGoWhereBackendApplication {
@@ -13,8 +17,6 @@ public class KakiGoWhereBackendApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRun() {
-        return args -> {};
     CommandLineRunner commandLineRun(PlaceService placeService) {
         return args -> {
 
