950e1ebe80c963e55d2d7d5fe72006d0ceb29fbe
adriantlh <adrian.tan.li.huat@gmail.com>
2025-08-08 13:50:44 +0800

    Added the service methods to update tourist's name and their interest category preference.

--- app/src/main/java/team2/kakigowherebackend/dto/TouristUpdateRequestDTO.java
+++ app/src/main/java/team2/kakigowherebackend/dto/TouristUpdateRequestDTO.java
@@ -3,7 +3,10 @@ package team2.kakigowherebackend.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class TouristUpdateRequestDTO {
     @NotBlank
     private String name;
@@ -11,16 +14,4 @@ public class TouristUpdateRequestDTO {
     @Size(max = 3)
     private List<Long> interestCategoryIds;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public List<Long> getInterestCategoryIds() {
-        return interestCategoryIds;
-    }
-    public void setInterestCategoryIds(List<Long> interestCategoryIds) {
-        this.interestCategoryIds = interestCategoryIds;
-    }
 }
